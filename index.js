2e4b07103d1448ddbb6c1d756b8dc70418fbcf47
Ankit Patwa <ankitpatwa2141@gmail.com>
2024-10-19 07:24:15 +0530

    19.10.2024

---
+++ index.js
@@ -0,0 +1,41 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+
+
+const server = new SMTPServer({
+    allowInsecureAuth : true,
+    authOptional : true,
+    onConnect(session, cb) {
+        /*
+        // this reject the connection
+        // cb(new Error('Cannot Accept the Connection'))
+
+        // this accept the Connection
+        // cb()
+        */
+
+        console.log(`onConnect`, session.id);
+        cb()
+
+
+    },
+    onMailFrom(address, session, cb) {
+        console.log('onMailFrom', address.address, session.id);
+        cb()
+    },
+    onRcptTo(address, session, cb){
+        console.log(`onRcptTo`, address, address.address, session.id)
+        cb()
+    },
+    onData(stream, session, cb){
+        stream.on(`data`, (data)=>console.log(`data -> ${data.toString()}`))
+        stream.on('end',cb)
+    }
+});
+
+
+server.on("error", (err) => {
+    console.log("Error %s", err.message);
+  });
+
+
+server.listen(25, () => console.log('Server Running on 25'))
