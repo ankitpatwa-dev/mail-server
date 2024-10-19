b7d54801f2453352473c32fae3ee213393cabf1c
Ankit Patwa <ankitpatwa2141@gmail.com>
2024-10-19 13:14:57 +0530

    19.10.2024

---
+++ send_mail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+// Configure Nodemailer to use the local SMTP server
+let transporter = nodemailer.createTransport({
+    host: 'localhost',  // Your SMTP server
+    port: 25,         // Port of your SMTP server
+    secure: false       // If true, use TLS; false otherwise
+});
+
+// Send an email
+async function sendTestEmail() {
+    try {
+        let info = await transporter.sendMail({
+            from: 'ak@theankit.xyz',    // Sender address
+            to: 'ankitpatwa2141@gmail.com',                    // Receiver address
+            subject: 'Hello from Node.js',                 // Email subject
+            text: 'This is a test email from Nodemailer!', // Email plain text body
+            html: '<b>This is a test email from Nodemailer!</b>' // Email HTML body
+        });
+
+        console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
+// Send the test email
+sendTestEmail();
+
